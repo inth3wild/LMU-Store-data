e475b153edeae6bbcc6ea5f420412c4f109e51ea
inth3wild <toochukeuchukwu@gmail.com>
2022-02-11 21:22:59 +0100

    added serial nnumbering feature for table

--- commands/save.js
+++ commands/save.js
@@ -51,7 +51,6 @@ function save(options) {
         // let userNames = prompt('Enter username(s) seperated by comma(,): ').split(',');
         // let passwords = prompt('Enter password(s) seperated by comma(,): ').split(',');
 
-
         if (userNames.length == passwords.length) {
             userNames.forEach((username, index) => {
                 let password = passwords[index]
--- modules/displayTable.js
+++ modules/displayTable.js
@@ -5,10 +5,11 @@ const chalk = require('chalk')
 
 async function displayTable(params) {
     let dbData = readfromDB();
+    let index = 0;
 
     // Instantiate command line table
     let table = new Table({
-        head: [chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
+        head: [chalk.white('S/N'), chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
     });
 
 
@@ -16,21 +17,23 @@ async function displayTable(params) {
         // Check if data is finished
         if (dbData[i].dataDigits[2].includes('-')) {
             table.push(
-                [ i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.red(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
+                [index+1, i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.red(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
             )
-            
+            index ++;
         } 
         // Check if credentials are invalid
         else if (dbData[i].dataDigits == 'Invalid Username/Password'){
             table.push(
-                [ i, chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), dbData[i].lastChecked ]
+                [index+1, i, chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), dbData[i].lastChecked ]
             )
+            index ++;
         }
-        // Then person has data
+            // Then person has data
         else {
             table.push(
-                [ i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.green(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
+                [index+1, i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.green(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
             )
+            index ++;
         }
         
     }
@@ -41,10 +44,11 @@ async function displayTable(params) {
 
 function displayTableWithData(params) {
     let dbData = readfromDB();
+    let index = 0;
 
     // Instantiate command line table
     let table = new Table({
-        head: [chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
+        head: [chalk.white('S/N'), chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
     });
 
 
@@ -59,8 +63,9 @@ function displayTableWithData(params) {
         // Then person has data
         else {
             table.push(
-                [ i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.green(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
+                [index+1, i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.green(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
             )
+            index++;
         }
         
     }
@@ -71,10 +76,11 @@ function displayTableWithData(params) {
 
 function displayTableWithoutData(params) {
     let dbData = readfromDB();
+    let index = 0;
 
     // Instantiate command line table
     let table = new Table({
-        head: [chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
+        head: [chalk.white('S/N'), chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
     });
 
 
@@ -82,9 +88,9 @@ function displayTableWithoutData(params) {
         // Check if data is finished
         if (dbData[i].dataDigits[2].includes('-')) {
             table.push(
-                [ i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.red(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
+                [index+1, i, dbData[i].dataDigits[0], dbData[i].dataDigits[1], chalk.red(dbData[i].dataDigits[2]), dbData[i].lastChecked ]
             )
-            
+            index++;
         } 
         // Check if credentials are invalid
         else if (dbData[i].dataDigits == 'Invalid Username/Password'){
@@ -101,10 +107,11 @@ function displayTableWithoutData(params) {
 
 function displayTableInvalid(params) {
     let dbData = readfromDB();
+    let index = 0;
 
     // Instantiate command line table
     let table = new Table({
-        head: [chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
+        head: [chalk.white('S/N'), chalk.white('Username'), chalk.white('Total Bandwidth'), chalk.white('Total Used'), chalk.white('Total Balance'), chalk.white('Last Checked')], 
     });
 
 
@@ -115,8 +122,9 @@ function displayTableInvalid(params) {
         // Check if credentials are invalid
         else if (dbData[i].dataDigits == 'Invalid Username/Password'){
             table.push(
-                [ i, chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), dbData[i].lastChecked ]
+                [index+1, i, chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), chalk.yellow(dbData[i].dataDigits), dbData[i].lastChecked ]
             )
+            index++;
         }
         // Then person has data
         else {
