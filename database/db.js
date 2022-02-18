14219fe3186adc092e67bbfeaba8d33d8c1e0b2e
Damilola Jerugba <damiisdandy@icloud.com>
2022-02-18 11:06:45 +0100

    made database hidden in home directory ✨

--- database/db.js
+++ database/db.js
@@ -1,42 +1,40 @@
-const JSONdb = require('simple-json-db');
-const db = new JSONdb('database.json');
+const JSONdb = require("simple-json-db");
+const homedir = require("os").homedir();
+const path = require("path");
 
+// globally store database file
+const db = new JSONdb(path.join(homedir, ".database.json"));
 
 function saveToDB(username, dataDigits, encryptedData) {
-    // Format current date in dd/mm/yy format
-    let today = new Date();
-    let dd = today.getDate();
-    let mm = today.getMonth() + 1;
-    let yy = today.getFullYear();
-    let todaysDate = dd + '/' + mm + '/' + yy;
+  // Format current date in dd/mm/yy format
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yy = today.getFullYear();
+  let todaysDate = dd + "/" + mm + "/" + yy;
 
+  db.set(username, {
+    dataDigits: dataDigits,
+    lastChecked: todaysDate,
+    encryptedData: encryptedData,
+  });
 
-    db.set(username, {
-        'dataDigits': dataDigits,
-        'lastChecked': todaysDate,
-        'encryptedData': encryptedData
-    })
-
-    return
+  return;
 }
-
 
 function readfromDB(params) {
-    let dbData = db.JSON();
-    return dbData
+  let dbData = db.JSON();
+  return dbData;
 }
-
 
 function removefromDB(username) {
-    db.delete(username);
+  db.delete(username);
 
-    return
+  return;
 }
-
-
 
 module.exports = {
-    saveToDB,
-    readfromDB,
-    removefromDB
-}
+  saveToDB,
+  readfromDB,
+  removefromDB,
+};
